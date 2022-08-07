f6d3a911cf29dc6087d4a7e61b4a895e917d20db
Ravi Udit <raviudit@gmail.com>
2022-08-06 21:58:38 -0400

    new project using javascript and html

---
+++ js/home.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    //alert("Ready to go!");
+    loadProducts();
+    hideMakeChangeButton();
+});
+
+function loadProducts(){
+
+    var itemNumber = 1;
+    var productRows = $('#productRows');
+
+    $.ajax({
+
+        type: 'GET',
+        url: 'http://vending.us-east-1.elasticbeanstalk.com/items',
+        success: function(productArray){
+            $.each(productArray, function(index, product){
+                var id = product.id;
+                var name = product.name;
+                var price = product.price;
+                var quantity = product.quantity;
+
+                var row = '<td><button type="button" class="btn btn-default">' + itemNumber + '<br>' +
+                                                                                 name + '<br> $' + 
+                                                                                 price + '<br>' +
+                                                                                 quantity + '</button>';
+                
+                productRows.append(row);
+                itemNumber++;
+            })
+
+        }
+    })
+}
+
+function hideMakeChangeButton(){    
+    $('#makeChangeButton').hide();
+}
+
+
+// Adding Money to the Vending Machine
+function addDollar(){
+    var money = Number($('#inputMoney').val());
+    var newMoney = money + 1.00;
+    $('#inputMoney').val(newMoney.toFixed(2));
+}
+
+function addQuarter(){
+    var money = Number($('#inputMoney').val());
+    var newMoney = money + 0.25;
+    $('#inputMoney').val(newMoney.toFixed(2));
+}
+
+function addDime(){
+    var money = Number($('#inputMoney').val());
+    var newMoney = money + 0.10;
+    $('#inputMoney').val(newMoney.toFixed(2));
+}
+
+function addNickel(){
+    var money = +$('#inputMoney').val();
+    var newMoney = money + 0.05;
+    $('#inputMoney').val(newMoney.toFixed(2));
+}
